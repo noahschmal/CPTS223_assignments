c21b6f043a79b79cc25e27e660e860b403187dda
noahschmal <noah.schmalenberger@wsu.edu>
2023-11-01 20:53:37 -0700

    initial

---
+++ PA3/build/CMakeFiles/PA3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PA3.dir/main.cpp.o"
+  "CMakeFiles/PA3.dir/main.cpp.o.d"
+  "PA3"
+  "PA3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PA3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
